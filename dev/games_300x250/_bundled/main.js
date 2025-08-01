075093630c3661313ec2fe3cc59b42315f3371da
Gar <gar@lonelydatum.com>
2025-08-01 21:36:53 +0100

    on

--- dev/games_300x250/_bundled/main.js
+++ dev/games_300x250/_bundled/main.js
@@ -94,41 +94,40 @@ exports.arcade = arcade;
 exports.theme2 = theme2;
 
 },{}],4:[function(require,module,exports){
-'use strict';
+"use strict";
 
-var _commonJsProlineJs = require('../../_common/js/proline.js');
+var _commonJsProlineJs = require("../../_common/js/proline.js");
 
-var _commonJsCommonJs = require('../../_common/js/common.js');
+var _commonJsCommonJs = require("../../_common/js/common.js");
 
-var _commonJsReadsJs = require('../../_common/js/reads.js');
+var _commonJsReadsJs = require("../../_common/js/reads.js");
 
 function init() {
+  var tl = (0, _commonJsCommonJs.commonInit)();
 
-	var tl = (0, _commonJsCommonJs.commonInit)();
+  // translate(-150px, -150px) scale(0.42)
 
-	// translate(-150px, -150px) scale(0.42)
+  tl.add("f2", "+=.5");
+  tl.to([".f1_device"], { duration: 0.3, scale: 0.39, y: -159 }, "f2");
 
-	tl.add("f2", "+=.5");
-	tl.to([".f1_device"], { duration: .3, scale: .39, y: -159 }, "f2");
+  tl.from([".f2_txt_1", ".f2_txt_2", ".f2_txt_3"], { duration: 0.3, opacity: 0 }, "f2");
+  tl.to([".f1_txt_1", ".f1_txt_2", ".f1_txt_3"], { duration: 0.3, opacity: 0 }, "f2");
 
-	tl.from([".f2_txt_1", ".f2_txt_2", ".f2_txt_3"], { duration: .3, opacity: 0 }, "f2");
-	tl.to([".f1_txt_1", ".f1_txt_2", ".f1_txt_3"], { duration: .3, opacity: 0 }, "f2");
+  tl.from(".f2_txt", { duration: 0.3, opacity: 0 });
 
-	tl.from(".f2_txt", { duration: .3, opacity: 0 });
+  tl.add("f3", "+=" + _commonJsReadsJs.games.t1);
+  tl.to(".f2_txt", { duration: 0.3, opacity: 0 }, "f3");
+  tl.from(".f3_txt", { duration: 0.3, opacity: 0 });
 
-	tl.add("f3", '+=' + _commonJsReadsJs.games.t1);
-	tl.to(".f2_txt", { duration: .3, opacity: 0 }, "f3");
-	tl.from(".f3_txt", { duration: .3, opacity: 0 });
+  tl.add("f4", "+=" + _commonJsReadsJs.games.t2);
+  tl.to(".f3_txt", { duration: 0.3, opacity: 0 }, "f4");
 
-	tl.add("f4", '+=' + _commonJsReadsJs.games.t2);
-	tl.to(".f3_txt", { duration: .3, opacity: 0 }, "f4");
+  tl.from(".f4_txt", { duration: 0.3, opacity: 0 });
+  tl.from(".f4_cta", { duration: 0.3, opacity: 0 });
 
-	tl.from(".f4_txt", { duration: .3, opacity: 0 });
-	tl.from(".f4_cta", { duration: .3, opacity: 0 });
+  tl.add((0, _commonJsProlineJs.olg)());
 
-	tl.add((0, _commonJsProlineJs.olg)());
-
-	return tl;
+  return tl;
 }
 
 init();
